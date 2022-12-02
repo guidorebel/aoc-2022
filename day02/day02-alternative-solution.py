42bee211cfeefe75590b051498e3bd8ce1edb1b8
gdrbl <github@guidorebel.nl>
2022-12-02 22:04:56 +0100

    day 2

---
+++ day02/day02-alternative-solution.py
@@ -0,0 +1,38 @@
+
+from aoc import PuzzleData
+
+p = PuzzleData("day02")
+data = p.rawdata()
+
+ROCK, PAPER, SCISSORS = [0, 1, 2]
+LOSS, DRAW, WIN       = [0, 3, 6]
+
+# Lookup a shape or result from the character in the data
+Char2Shape  = {"A":ROCK, "B":PAPER, "C":SCISSORS, "X":ROCK, "Y":PAPER, "Z":SCISSORS}
+Char2Result = {"X":LOSS, "Y":DRAW, "Z":WIN}
+
+# Truth tables
+scoreFromShape  = [DRAW, LOSS, WIN,   WIN, DRAW, LOSS,   LOSS, WIN, DRAW]
+shapeFromResult = [SCISSORS, ROCK, PAPER,   ROCK,PAPER, SCISSORS,   PAPER, SCISSORS, ROCK]
+
+score1 = 0  # For part 1
+score2 = 0  # For part 2
+
+for round in data:
+
+    elf, me     = round.split()  # For part 1
+    elf, result = round.split()  # For part 2
+
+    me     = Char2Shape[me]
+    elf    = Char2Shape[elf]
+    result = Char2Result[result]
+
+    score1 += me+1
+    score1 += scoreFromShape [me*3+elf]
+
+    me = shapeFromResult[result+elf]
+    score2 += me+1
+    score2 += scoreFromShape [me*3+elf]
+
+print (score1) # For part 1
+print (score2) # For part 2
