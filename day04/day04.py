60ac15d141219d01cb8002eb45f178ab55aab564
gdrbl <github@guidorebel.nl>
2022-12-04 16:53:46 +0100

    day 4

---
+++ day04/day04.py
@@ -0,0 +1,27 @@
+
+from aoc import PuzzleData
+
+p = PuzzleData("day04")
+data = p.rawdata()
+
+count1 = 0
+count2 = 0
+
+for assignment in data:
+
+    c1, c2 = assignment.split(",")
+    c1start, c1stop = [int(i) for i in c1.split("-")]
+    c2start, c2stop = [int(i) for i in c2.split("-")]
+    
+    if c1start <= c2start <= c1stop and c1start <= c2stop <= c1stop:
+        count1+=1
+    elif c2start <= c1start <= c2stop and c2start <= c1stop <= c2stop:
+        count1+=1
+
+    if c1start <= c2start <= c1stop or c1start <= c2stop <= c1stop:
+        count2+=1
+    elif c2start <= c1start <= c2stop or c2start <= c1stop <= c2stop:
+        count2+=1
+
+print (count1)
+print (count2)
