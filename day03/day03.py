da3f094ebb2ddd405d6f0efe44cbda5a491b5165
gdrbl <github@guidorebel.nl>
2022-12-04 16:44:03 +0100

    day 3

---
+++ day03/day03.py
@@ -0,0 +1,56 @@
+
+from aoc import PuzzleData
+
+p = PuzzleData("day03")
+data = p.rawdata()
+
+
+total = 0
+
+for rucksack in data:
+
+    compartmentItems = int(len(rucksack)/2)
+    c1 = rucksack[:compartmentItems]
+    c2 = rucksack[compartmentItems:]
+
+    item = None
+    for c in c1:
+        if c in c2:
+            item = c
+            break
+    
+    if item:
+        if item.isupper(): score = ord(item)-ord("A")+27
+        else:              score = ord(item)-ord("a")+1
+
+    total += score
+    print (f"{item} : {score} -> {total}")
+
+
+print("")
+
+
+total=0
+
+nRucksack = int(len(data)/3)
+
+for i in range(nRucksack):
+
+    r1 = data[i*3+0]
+    r2 = data[i*3+1]
+    r3 = data[i*3+2]
+
+    item = None
+    for c in r1:
+        if c in r2:
+            if c in r3:
+                item = c
+                break
+        
+    if item:
+        if item.isupper(): score = ord(item)-ord("A")+27
+        else:              score = ord(item)-ord("a")+1
+
+    total += score
+    print (f"{item} : {score} -> {total}")
+
