43362527f62b7c35cba4d5bb640e5a0b54356c95
gdrbl <github@guidorebel.nl>
2022-12-05 19:11:19 +0100

    day 5

---
+++ day03/aoc.py
@@ -0,0 +1,23 @@
+
+
+class PuzzleData():
+
+    def __init__(self, day, example=False) -> None:
+        
+        self.day = day
+
+        datafile = day
+        if example: datafile+="-example"
+        self.input = f"input\\{datafile}.txt"
+
+        
+    def rawdata(self):
+
+        data=[]
+
+        with open(self.input, "r") as f:
+            for line in f.readlines():
+                data.append(line.strip())
+        
+        return data
+
