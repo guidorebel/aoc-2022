b49c2ad99c44a8b31803788a05a435247b94c458
gdrbl <github@guidorebel.nl>
2022-12-06 11:43:21 +0100

    day 6

--- day06/day06.py
+++ day06/day06.py
@@ -1,8 +1,26 @@
 
 from aoc import PuzzleData
 
-from collections import defaultdict
-
 puzzle = PuzzleData(__file__)
-data = puzzle.rawdata()
+data = puzzle.rawdata()[0]
 
+def findFirstMarker (data, length):
+
+    marker = None
+    i = 0
+
+    while i < len(data)-length:
+
+        chunk = data [i:i+length]   # Get a chunk from the data
+        chunk = "".join(set(chunk)) # Remove all duplicate characters
+
+        if len(chunk) == length:
+            marker = i+length
+            break
+
+        i+=1
+
+    return marker
+    
+print (findFirstMarker (data, 4))   # Part 1
+print (findFirstMarker (data, 14))  # Part 2
