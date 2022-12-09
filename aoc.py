898ed374cc35345eec54a57fe9af3f8bd634166e
gdrbl <github@guidorebel.nl>
2022-12-09 22:42:56 +0100

    Update docstrings

--- aoc.py
+++ aoc.py
@@ -1,31 +1,51 @@
 
 import os
+import math
+
 
 class PuzzleData:
 
+    """ Puzzle data class for Advent of Code """
+
     def __init__(self, scriptfname:str) -> None:
 
+        """
+        Create the data class with a reference to the script file (for example, use ___file___).
+        The data file is expected with the same basename but the extention .txt and in the folder ..\input.
+        For example: a reference to \day05\day05.py will expect the data file in \input\day05.txt.
+        """
+
+        self.today: str
         self.today = os.path.basename(scriptfname)
         self.today = self.today.replace(".py", "")
 
+        self.scriptdir: str
         self.scriptdir = os.path.dirname(scriptfname)
+
+        self.datafile: str
+        self.datafile = self.today+".txt"
+        self.datafile = os.path.join (self.scriptdir, "..\\input\\", self.datafile)
+        self.datafile = os.path.abspath (self.datafile)
 
 
     def getData(self) -> str:
 
-        datafile = self.today
-        datafile += ".txt"
-
-        datafile = os.path.join (self.scriptdir, "..\\input\\", datafile)
-        datafile = os.path.abspath (datafile)
+        """
+        Get the raw data from the file.
+        """
 
         data: str = ""
-        with open (datafile, "r") as f:
+        with open (self.datafile, "r") as f:
             data = f.read()
         return data
 
 
     def getStrList(self) -> list[str]:
+
+        """
+        Get the data as a list of strings.
+        Each line in the data file is an item in the list.
+        """
 
         data=self.getData()
         return data.split("\n")
@@ -33,9 +53,14 @@ class PuzzleData:
 
     def getIntGrid(self) -> list[list[int]]:
 
+        """
+        Get the data as a grid (list of lists) of integers.
+        """
+
         data = self.getData()
         datalist = data.split("\n")
 
         grid = []
         for line in datalist: grid.append([int(i) for i in line])
         return grid
+
